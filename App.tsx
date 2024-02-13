1672226e194ce06d1e2eb70e8d8722dc1307f708
birukmk <birukawimekonnen@gmail.com>
2024-02-13 11:46:37 +0300

    little bit better ui

--- App.tsx
+++ App.tsx
@@ -1,5 +1,13 @@
 import React, {useState} from 'react';
-import {Text, View, TextInput, Button} from 'react-native';
+import {
+  Text,
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  ImageBackground,
+  ScrollView,
+} from 'react-native';
 import {keyGenerator} from './keyGenerator';
 import {cipherGenerator} from './cipherGenerator';
 import {cipherDecryptor} from './cipherDecryptor';
@@ -45,12 +53,8 @@ const DesApp = () => {
   };
 
   const handledecryptChange = () => {
-    if (!Generatedkeys) {
-      setalert('you need to create a cipher first');
-    } else {
-      decrypted_text = cipherDecryptor(E_msg, keyforDEC, pad);
-      setdecrypted_msg(decrypted_text);
-    }
+    decrypted_text = cipherDecryptor(E_msg, keyforDEC, pad);
+    setdecrypted_msg(decrypted_text);
   };
 
   const handleContentSizeChange = (event: {
@@ -60,44 +64,86 @@ const DesApp = () => {
   };
 
   return (
-    <View>
-      <Text style={{fontSize: 30, fontWeight: 'bold'}}>
-        DES Encryption / Decryption
-      </Text>
+    <View style={styles.container}>
+      <ImageBackground source={require('./wood.jpg')} style={styles.navBar}>
+        <Text style={styles.navBarTitle}>DES Encryption / Decryption</Text>
+      </ImageBackground>
+      <ScrollView>
+        <View style={styles.E_container}>
+          <Text style={styles.title}>ENCRYPTION </Text>
+          <Text>YOUR MESSAGE</Text>
+          <TextInput
+            placeholder="Enter Text"
+            value={text}
+            onChangeText={text => setText(text)}
+            onContentSizeChange={handleContentSizeChange}
+            style={{height: inputHeight, width: 400, fontWeight: 'bold'}}
+            multiline={true}
+          />
+          <View style={styles.key}>
+            <Text>YOUR SECRET KEY :</Text>
+            <TextInput
+              placeholder="Enter secret key"
+              value={key}
+              onChangeText={key => setKey(key)}
+              maxLength={8}
+              style={{fontWeight: 'bold', width: 250}}
+            />
+          </View>
 
-      <Text style={{fontSize: 20, fontWeight: 'bold'}}>Encryption </Text>
-
-      <TextInput
-        placeholder="Enter Text"
-        value={text}
-        onChangeText={text => setText(text)}
-        onContentSizeChange={handleContentSizeChange}
-        style={{height: inputHeight, borderColor: 'gray', borderWidth: 1}}
-        multiline={true}
-      />
-
-      <TextInput
-        placeholder="Enter secret key"
-        value={key}
-        onChangeText={key => setKey(key)}
-        maxLength={8}
-      />
-
-      <Text>{K_msg}</Text>
-      <Button title="Generate Cipher" onPress={handleKeyChange} />
-      <Text>{E_msg}</Text>
-      <Text style={{fontSize: 20, fontWeight: 'bold'}}>Decryption </Text>
-      <TextInput
-        placeholder={E_msg}
-        value={E_msg_copy}
-        onChangeText={E_msg_copy => set_E_msg_copy(E_msg_copy)}
-        onContentSizeChange={handleContentSizeChange}
-        style={{height: inputHeight, borderColor: 'gray', borderWidth: 1}}
-      />
-      <Button title="Decrypt Cipher" onPress={handledecryptChange} />
-      <Text>{decrypted_msg}</Text>
+          <Text style={{color: 'red'}}>{K_msg}</Text>
+          <Button title="Generate Cipher" onPress={handleKeyChange} />
+          <Text style={{width: 400}}>{E_msg}</Text>
+        </View>
+        <View style={styles.E_container}>
+          <Text style={styles.title}>DECRYPTION </Text>
+          <Text>YOUR MESSAGE</Text>
+          <TextInput
+            placeholder={E_msg}
+            value={E_msg_copy}
+            onChangeText={E_msg_copy => set_E_msg_copy(E_msg_copy)}
+            onContentSizeChange={handleContentSizeChange}
+            style={{height: inputHeight, width: 400, fontWeight: 'bold'}}
+            multiline={true}
+          />
+          <Button title="Decrypt Cipher" onPress={handledecryptChange} />
+          <Text style={{width: 400}}>{decrypted_msg}</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  E_container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  key: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  navBar: {
+    height: 60,
+    backgroundColor: 'grey',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  navBarTitle: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  title: {
+    color: 'grey',
+    fontSize: 25,
+    padding: 5,
+  },
+});
 
 export default DesApp;
